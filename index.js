f2eaf73c14469217899383c6d11433f44c1e4422
bulps2911 <bulps2911@gmail.com>
2019-11-25 18:31:49 +0800

    fixed changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 const express = require("express");
 const fs = require("fs");
 const Jimp = require("jimp");
-const gsap = require("gsap");
+// const gsap = require("gsap");
 
 const dotenv = require('dotenv');
 dotenv.config();
