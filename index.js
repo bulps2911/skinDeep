1b51282bf44d6c196f799f6310c6c3c431599849
bulps2911 <bulps2911@gmail.com>
2019-12-11 10:01:27 +0800

    used env variable

--- index.js
+++ index.js
@@ -11,15 +11,14 @@ const fileupload = require("express-fileupload");
 const multer = require("multer");
 // const gsap = require("gsap");
 
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 //connect to db
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 //make a connection
 const PASSWORD = process.env.PASSWORD || 3000;
-const uri =
-  "mongodb+srv://dbAdmin:98765A321@manacluster-siac4.mongodb.net/skinDeep";
+const uri = `mongodb+srv://dbAdmin:${PASSWORD}@manacluster-siac4.mongodb.net/skinDeep`;
 // //downgraded mongodb driver
 // const uri = "mongodb://dbAdmin:98765A321@manacluster-shard-00-00-siac4.mongodb.net:27017,manacluster-shard-00-01-siac4.mongodb.net:27017,manacluster-shard-00-02-siac4.mongodb.net:27017/skinDeep"
 mongoose.connect(uri, { useNewUrlParser: true });
@@ -29,11 +28,9 @@ require("./models/UserData");
 const prod = require("./getprods.js");
 const upload = require("./upload.js");
 
-
 const userData = mongoose.model("UserData");
 const { MongoClient } = require("mongodb");
 const app = express();
-
 
 const PORT = process.env.PORT || 3000;
 
@@ -48,10 +45,10 @@ const client = new MongoClient(uri, {
 });
 
 //initiate cors to allow access control origin on client
-const corsOptions={
+const corsOptions = {
   origin: "*",
-  optionsSuccessStatus:200,
-}
+  optionsSuccessStatus: 200
+};
 app.use(cors(corsOptions));
 app.use(fileupload());
 
@@ -64,20 +61,20 @@ app.get("/", (req, res) => {
   res.render("upload.pug");
 });
 
-app.post("/uploadapi", (req, res, next)=>{
+app.post("/uploadapi", (req, res, next) => {
   // const file = req.files;
   // console.log(file);
-  const file= req.files.file;
+  const file = req.files.file;
   console.log(file);
-  file.mv("./public/"+file.name, (err, result)=>{
-    if(err){
+  file.mv("./public/" + file.name, (err, result) => {
+    if (err) {
       console.log(err);
-      return res.status(500).json(err)
+      return res.status(500).json(err);
       // res.status(400).send("Failed1: No image uploaded");
-    } else{
+    } else {
       console.log("uploading");
       console.log(req.files);
-      
+
       //create new record in mongodb
       const fullPath = "public/" + file.name;
       const b64head = "data:image/jpeg;base64,";
@@ -85,51 +82,47 @@ app.post("/uploadapi", (req, res, next)=>{
         "base64"
       );
       const b64 = b64head + converted;
-      const imgrgb=[];
+      const imgrgb = [];
       //jimp is a promise. must finish processing first before running next thing
-      Jimp.read(fullPath, (err, img)=>{
-        if (err){
+      Jimp.read(fullPath, (err, img) => {
+        if (err) {
           console.log("jimp cannot read img");
           console.log(err);
-        } else{
-          img.resize(256,256)
-          .scan(0,0,img.bitmap.width, img.bitmap.height,(x,y,idx)=>{
-            const red = img.bitmap.data[idx+0];
-            const green = img.bitmap.data[idx+1];
-            const blue = img.bitmap.data[idx+2];
-            if (x==img.bitmap.width -1 && y==img.bitmap.height -1){
-              imgrgb.push(red.toString(), green.toString(), blue.toString());
-              const document = {
-                imgUrl: b64,
-                rgb: imgrgb
-              };
-              const photo = new userData(document);
-              photo.save((err, userData) => {
-                try{
-                  console.log("SUCCESS! img uploaded to db");
-                  res.status(200).send({
-                    success: true, 
-                    message: "File Uploaded!",
-                    rgb: photo.rgb
-                  });
-                }catch(err){
-                  console.log(err);
-                  res.send("Failed");
-
-                }
-                
-              });
-              
-            }
-          });
+        } else {
+          img
+            .resize(256, 256)
+            .scan(0, 0, img.bitmap.width, img.bitmap.height, (x, y, idx) => {
+              const red = img.bitmap.data[idx + 0];
+              const green = img.bitmap.data[idx + 1];
+              const blue = img.bitmap.data[idx + 2];
+              if (x == img.bitmap.width - 1 && y == img.bitmap.height - 1) {
+                imgrgb.push(red.toString(), green.toString(), blue.toString());
+                const document = {
+                  imgUrl: b64,
+                  rgb: imgrgb
+                };
+                const photo = new userData(document);
+                photo.save((err, userData) => {
+                  try {
+                    console.log("SUCCESS! img uploaded to db");
+                    res.status(200).send({
+                      success: true,
+                      message: "File Uploaded!",
+                      rgb: photo.rgb
+                    });
+                  } catch (err) {
+                    console.log(err);
+                    res.send("Failed");
+                  }
+                });
+              }
+            });
           console.log("img resized");
         }
       });
-
     }
   });
-  
-})
+});
 
 app.post("/upload", (req, res) => {
   console.log("uploading..");
@@ -145,46 +138,45 @@ app.post("/upload", (req, res) => {
         "base64"
       );
       const b64 = b64head + converted;
-      const imgrgb=[];
+      const imgrgb = [];
       //jimp is a promise. must finish processing first before running next thing
-      Jimp.read(fullPath, (err, img)=>{
-        if (err){
+      Jimp.read(fullPath, (err, img) => {
+        if (err) {
           console.log("jimp cannot read img");
           console.log(err);
-        } else{
-          img.resize(256,256)
-          .scan(0,0,img.bitmap.width, img.bitmap.height,(x,y,idx)=>{
-            const red = img.bitmap.data[idx+0];
-            const green = img.bitmap.data[idx+1];
-            const blue = img.bitmap.data[idx+2];
-            const alpha = img.bitmap.data[idx+3];
-            if (x==img.bitmap.width -1 && y==img.bitmap.height -1){
-              imgrgb.push(red.toString(), green.toString(), blue.toString());
-              const document = {
-                imgUrl: b64,
-                rgb: imgrgb
-              };
-              
-              const photo = new userData(document);
-        
-              photo.save((err, userData) => {
-                if (err) {
-                  console.log(err);
-                } else {
-                  console.log("SUCCESS! img uploaded to db");
-                  res.render("result.pug", {rgb : photo.rgb});
-                }
-              });
-              
-            }
-          });
+        } else {
+          img
+            .resize(256, 256)
+            .scan(0, 0, img.bitmap.width, img.bitmap.height, (x, y, idx) => {
+              const red = img.bitmap.data[idx + 0];
+              const green = img.bitmap.data[idx + 1];
+              const blue = img.bitmap.data[idx + 2];
+              const alpha = img.bitmap.data[idx + 3];
+              if (x == img.bitmap.width - 1 && y == img.bitmap.height - 1) {
+                imgrgb.push(red.toString(), green.toString(), blue.toString());
+                const document = {
+                  imgUrl: b64,
+                  rgb: imgrgb
+                };
+
+                const photo = new userData(document);
+
+                photo.save((err, userData) => {
+                  if (err) {
+                    console.log(err);
+                  } else {
+                    console.log("SUCCESS! img uploaded to db");
+                    res.render("result.pug", { rgb: photo.rgb });
+                  }
+                });
+              }
+            });
           console.log("img resized");
         }
       });
     }
   });
 });
-
 
 app.get("/result", (req, res) => {
   console.log("GET request for results");
@@ -197,21 +189,19 @@ app.get("/productsapi", async (req, res) => {
   try {
     finalResult = await prod(client);
     //const finalString=[];
-    const finalshade=  await `${finalResult.map(each => each.shadeName)}`;
+    const finalshade = await `${finalResult.map(each => each.shadeName)}`;
     const finalString = await `${finalResult.map(each => each.imgUrl)}`;
     //console.log(finalString);
     const arrayfs = finalString.split(",");
     // console.log(finalString);
     console.log(finalResult);
-    res.status(200).json({finalResult});
+    res.status(200).json({ finalResult });
     // res.status(200).render("index.pug", { product: arrayfs });
-
   } catch (error) {
     finalResult = [];
     res.status(400).send("Failed");
   }
 });
-
 
 app.get("/products", async (req, res) => {
   console.log("GET request for products");
@@ -219,7 +209,7 @@ app.get("/products", async (req, res) => {
   try {
     finalResult = await prod(client);
     //const finalString=[];
-    const finalshade=  await `${finalResult.map(each => each.shadeName)}`;
+    const finalshade = await `${finalResult.map(each => each.shadeName)}`;
     const finalString = await `${finalResult.map(each => each.imgUrl)}`;
     //console.log(finalString);
     const arrayfs = finalString.split(",");
@@ -227,7 +217,6 @@ app.get("/products", async (req, res) => {
     console.log(finalResult);
     // res.status(200).json({finalResult});
     res.status(200).render("index.pug", { product: arrayfs });
-
   } catch (error) {
     finalResult = [];
     res.status(400).send("Failed");
